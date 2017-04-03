0ef55a3e3c0972b30f9e5ac6858222099578faa9
Lucas Guilherme <lguilhermee@users.noreply.github.com>
2017-04-03 18:52:44 -0300

    Update jogoLabirinto.cpp

--- jogoLabirinto.cpp
+++ jogoLabirinto.cpp
@@ -2,26 +2,84 @@
 #include <stdio.h>
 #include <windows.h>
 
+int x, y, antigoX, antigoY; // PRECISAMOS DO ANTIGO X E Y PARA APAGAR NOSSO RASTRO ANTERIOR
+int colunaVertical, colunaHorizontal; // Variaveis que guardam o tamanho total do console
 
-int x,y, oldX, oldY; // Variaveis que serão utilizadas depois.
 
-// Imprime na tela em determinado X localização
+// Com esta função é possível pegar o tamanho total das tela. Os valores da Coluna estão nas variaveis globais acima.
+void tamanhoTotalDaTela(){
+
+CONSOLE_SCREEN_BUFFER_INFO bufferConsole;
+
+GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &bufferConsole); // Passamos a variavel bufferConsole para esta função
+colunaVertical = bufferConsole.srWindow.Right - bufferConsole.srWindow.Left + 1; // Pega tamanho total de linhas na vertical
+colunaHorizontal = bufferConsole.srWindow.Bottom - bufferConsole.srWindow.Top + 1; // Pega tamanho total de linhas horizontal
+}
+
+
+// Com esta função nós conseguimos nos mover pelo console utilizando X e Y
 void movePOS(int x, int y) {
 
     COORD coords; // Classe COORD (Windows.h)
     coords.X = x; //Coordenadas X (Horizontal)
     coords.Y = y; //Coordenads Y (Vertical)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coords);
+    printf("%c", 254); //Printa um quadrado na determinada posição do console.
+
 }
 
 
-//Função principal
+
+int menuInicial () {
+
+    /*MENU DEVE CONTER AS SEGUINTER INFORMAÇÕES
+     * - IMAGEM DE APRESENTAÇÃO EM ASCII
+     * - NOMES DOS DESENVOLVEDORES
+     * - VERSÃO DO JOGO
+     * - MENU DE OPÇÕES [ INICIAR JOGO, MOSTRAR RECORDE, SAIR DO JOGO ]
+    */
+
+    //Está função deverá retornar um int ou um char
+
+    return 1;
+}
+
+
+char menuFinal() {
+
+
+    /*MENU FINAL DEVE CONTER AS SEGUINTES INFORMAÇÕES
+     * - IMAGEM DE GAME OVER EM ASCII
+     * - JOGADOR NOVAMENTE
+     * - VOLTAR AO MENU PRINCIPAL
+     * - SAIR DO JOGO
+     * */
+
+    // O MENU DEVE RETORNAR UM CHAR OU UM INTEIRO
+
+    return 'a';
+}
+
+void geradorDeMapa() {
+
+    // AINDA DEVE SER PENSADO COMO SER GERADO.
+    // PROVAVELMENTE COM A FUNÇÃO RAND.
+}
+
+
 int main() {
 
-    //CHAMADA DA FUNCAO e PASSANDO VALROES X E Y;
-    movePOS(40,160);
-    printf("x");
+    /*EXEMPLO DE COORDENADAS
+     * O SEGUINTE CÓDIGO ABAIXO COLETA O TAMANHO DO CONSOLE ATRAVÉS
+     * DA FUNÇÃO tamanhoTotalTabela(); E LOGO APÓS COM A FUNÇÃO
+     * movePOS(y,x) NÓS NOS MOVEMOS PARA O MEIO DA TELA
+     * UTILIZANDO AS VARIAVEIS GLOBAIS COM O TAMANHO DO CONSOLE
+     * GERADO PELA FUNÇÃO tamanhoTotalTabela() ANTERIORMENTE.
+     */
 
+    tamanhoTotalDaTela();
+    movePOS((colunaVertical/2)-20,colunaHorizontal/2);
+    
     system("pause");
 }
 
